bc87fe164bf8759e2fc4d0ed47247196661aedfa
Ofer Helman <ofer.helman@kaltura.com>
2023-08-31 09:51:21 +0300

    tests
    Signed-off-by: Ofer Helman <ofer.helman@kaltura.com>

---
+++ tests/sanity.py
@@ -0,0 +1,14 @@
+def test_basic():
+    from pydatic_prompter import Prompter
+    from pydantic import BaseModel
+
+    class Hi(BaseModel):
+        response: str
+
+    @Prompter(llm="openai", jinja=False, model_name="gpt-3.5-turbo")
+    def funct(hello) -> Hi:
+        """
+        >> user: say {hello}
+        """
+
+    funct.build_string(hello="hi")
