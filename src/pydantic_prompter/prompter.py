f08300911e2a18a1dd79c0cce5635f758a1bc210
Ofer Helman <ofer.helman@kaltura.com>
2023-09-02 22:36:57 +0300

    support bedrock
    Signed-off-by: Ofer Helman <ofer.helman@kaltura.com>

--- src/pydantic_prompter/prompter.py
+++ src/pydantic_prompter/prompter.py
@@ -1,7 +1,10 @@
+import json
 import logging
 import os
+import textwrap
 from typing import Dict, List, Any
 
+import boto3
 import yaml
 from jinja2 import Template
 from openai.error import AuthenticationError
@@ -11,7 +14,9 @@ from retry import retry
 from pydantic_prompter.exceptions import (
     OpenAiAuthenticationError,
     Retryable,
-    FailedToCastLLMResult, NonRetryable, )
+    FailedToCastLLMResult,
+    NonRetryable,
+)
 
 logger = logging.getLogger()
 
@@ -28,8 +33,19 @@ class LLM:
     def __init__(self, model_name):
         self.model_name = model_name
 
+    def debug_prompt(self, messages: List[Message], scheme: dict):
+        raise NotImplementedError
+
     def call(self, messages: List[Message], scheme: Dict) -> str:
         raise NotImplementedError
+
+    @classmethod
+    def get_llm(cls, llm: str, model_name: str):
+        if llm == "openai":
+            return OpenAI(model_name)
+        elif llm == "bedrock" and model_name.startswith("anthropic"):
+            return BedRockAnthropic(model_name)
+        raise Exception(f"Model not implemented {llm}, {model_name}")
 
 
 class OpenAI(LLM):
@@ -37,6 +53,9 @@ class OpenAI(LLM):
     def to_openai_format(msgs: List[Message]):
         openai_msgs = [item.dict() for item in msgs]
         return openai_msgs
+
+    def debug_prompt(self, messages: List[Message], scheme: dict) -> str:
+        return "\n".join(map(str, self.to_openai_format(messages)))
 
     def call(self, messages: List[Message], scheme: dict) -> str:
         import openai
@@ -56,6 +75,64 @@ class OpenAI(LLM):
         except AuthenticationError as e:
             raise OpenAiAuthenticationError(e)
         return chat_completion.choices[0].message["function_call"]["arguments"]
+
+
+class BedRockAnthropic(LLM):
+    def debug_prompt(self, messages: List[Message], scheme: dict) -> str:
+        return self.build_prompt(messages, scheme)
+
+    @staticmethod
+    def to_anthropic_format(msgs: List[Message]):
+        role_converter = {"user": "Human", "system": "Human", "assistant": "Assistant"}
+        output = []
+        for msg in msgs:
+            output.append(f"{role_converter[msg.role]}: {msg.content}")
+        return "\n".join(output)
+
+    def build_prompt(self, messages: List[Message], scheme: dict):
+        ant_template = textwrap.dedent(
+            """Human: You are a REST API that answers the question contained in <question> tags.
+Your response should be in the JSON which it's schema is specified in the <json> tags.
+
+<json>
+{{ schema }}
+</json>
+<question>
+{{ question }}
+</question>
+
+Assistant:
+"""
+        )
+        ant_scheme = json.dumps(scheme["parameters"]["properties"], indent=4)
+        ant_msgs = self.to_anthropic_format(messages)
+        template = Template(ant_template, keep_trailing_newline=True)
+        content = template.render(schema=ant_scheme, question=ant_msgs).strip()
+        return content
+
+    def call(self, messages: List[Message], scheme: dict) -> str:
+        content = self.build_prompt(messages, scheme)
+
+        body = json.dumps(
+            {
+                "max_tokens_to_sample": 200,
+                "prompt": content,
+                "stop_sequences": ["Human:"],
+                "temperature": 0.7,
+            }
+        )
+        logger.info(body)
+        session = boto3.Session(profile_name="okta_prod", region_name="us-east-1")
+        client = session.client("bedrock")
+        response = client.invoke_model(
+            body=body,
+            modelId="anthropic.claude-instant-v1",
+            accept="application/json",
+            contentType="application/json",
+        )
+        response_body = json.loads(response.get("body").read().decode())
+        logger.info(response_body)
+        return response_body.get("completion")
 
 
 class AnnotationParser:
@@ -122,7 +199,7 @@ class _Pr:
 
     def build_string(self, **inputs):
         msgs = self.build_prompt(**inputs)
-        return "\n".join(map(str, msgs))
+        return self.llm.debug_prompt(msgs, self.parser.llm_schema())
 
     def build_prompt(self, **inputs) -> List[Message]:
         if self.jinja:
@@ -132,7 +209,8 @@ class _Pr:
             content = self.function.__doc__.format(**inputs)
 
         import re
-        pattern = r'\n\s*- '
+
+        pattern = r"\n\s*- "
         parts = re.split(pattern, content)
 
         messages = []
@@ -155,8 +233,7 @@ class _Pr:
 class Prompter:
     def __init__(self, llm: str, model_name: str, jinja=False):
         self.jinja = jinja
-        if llm == "openai":
-            self.llm = OpenAI(model_name)
+        self.llm = LLM.get_llm(llm=llm, model_name=model_name)
 
     def __call__(self, function):
         return _Pr(function=function, jinja=self.jinja, llm=self.llm)
--- tests/gpt_tests.py
+++ tests/gpt_tests.py
@@ -1,7 +1,8 @@
 import logging
 import os
 from typing import List
-
+import textwrap
+import boto3
 from pydantic import BaseModel, Field
 
 from pydantic_prompter import Prompter
@@ -12,6 +13,13 @@ logging.basicConfig()
 run_gpt = Settings().openai_api_key is not None
 if run_gpt:
     os.environ["OPENAI_API_KEY"] = Settings().openai_api_key
+
+run_bedrock = True
+try:
+    session = boto3.Session(profile_name="okta_prod", region_name="us-east-1")
+    client = session.client("bedrock")
+except:
+    run_bedrock = False
 
 
 def test_pydantic():
@@ -28,12 +36,16 @@ def test_pydantic():
             what is my name and my children name
         """
 
-    expected = [Message(role='system', content='you are a writer'),
-                Message(role='user', content='hi, my name is Ofer and my children are called, aa, bb, cc'),
-                Message(role='user', content='what is my name and my children name')]
+    expected = [
+        Message(role="system", content="you are a writer"),
+        Message(
+            role="user",
+            content="hi, my name is Ofer and my children are called, aa, bb, cc",
+        ),
+        Message(role="user", content="what is my name and my children name"),
+    ]
     res = bbb.build_prompt(name="Ofer")
     assert res == expected
-
     if run_gpt:
         res: Hey = bbb(name="Ofer")
         assert isinstance(res, Hey)
@@ -53,8 +65,13 @@ def test_generic():
             how many children do I have?
         """
 
-    expected = [Message(role='user', content='hi, my name is Ofer  and my children are called, aa, bb, cc'),
-                Message(role='user', content='how many children do I have?')]
+    expected = [
+        Message(
+            role="user",
+            content="hi, my name is Ofer  and my children are called, aa, bb, cc",
+        ),
+        Message(role="user", content="how many children do I have?"),
+    ]
     res = aaa.build_prompt(name="Ofer")
     assert res == expected
     if run_gpt:
@@ -85,13 +102,105 @@ def test_non_yaml():
     ]
     res = search_query.build_prompt(history="\n".join(history))
     expected = [
-        Message(role='assistant', content='what genre do you want to watch?'),
-        Message(role='user', content='Comedy'),
-        Message(role='assistant', content='do you want a movie or series?'),
-        Message(role='user', content='Movie'),
-        Message(role='user',
-                content='Generate a Google-like search query text encompassing all previous chat questions and answers')]
+        Message(role="assistant", content="what genre do you want to watch?"),
+        Message(role="user", content="Comedy"),
+        Message(role="assistant", content="do you want a movie or series?"),
+        Message(role="user", content="Movie"),
+        Message(
+            role="user",
+            content="Generate a Google-like search query "
+            "text encompassing all previous chat questions and answers",
+        ),
+    ]
     assert res == expected
     if run_gpt:
+        res = search_query(history="\n".join(history))
+        assert isinstance(res, QueryGPTResponse)
+
+
+def test_anthropic_generic():
+    class MyChildren(BaseModel):
+        num_of_children: int
+        children_names: List[str] = Field(description="The names of my children")
+
+    @Prompter(llm="bedrock", model_name="anthropic.claude-instant-v1")
+    def aaa(name) -> MyChildren:
+        """
+        - user: hi, my name is {name} and my children are called, aa, bb, cc
+        - user: |
+            how many children do I have and what's their names?
+        """
+
+    expected = [
+        Message(
+            role="user",
+            content="hi, my name is Ofer and my children are called, aa, bb, cc",
+        ),
+        Message(
+            role="user", content="how many children do I have and what's their names?"
+        ),
+    ]
+    res = aaa.build_prompt(name="Ofer")
+    assert res == expected
+    if run_bedrock:
+        res: MyChildren = aaa(name="Ofer")
+        assert isinstance(res, MyChildren)
+        assert res.num_of_children == 3
+        assert res.children_names == ["aa", "bb", "cc"]
+
+
+def test_anthropic_non_yaml():
+    class QueryGPTResponse(BaseModel):
+        google_like_search_term: str
+
+    @Prompter(llm="bedrock", model_name="anthropic.claude-instant-v1", jinja=True)
+    def search_query(history) -> QueryGPTResponse:
+        """
+        {{ history }}
+
+        - user: |
+            Generate a Google-like search query text encompassing all previous chat questions and answers
+        """
+
+    history = [
+        "- assistant: what genre do you want to watch?",
+        "- user: Comedy",
+        "- assistant: do you want a movie or series?",
+        "- user: Movie",
+    ]
+    res = search_query.build_prompt(history="\n".join(history))
+    expected = [
+        Message(role="assistant", content="what genre do you want to watch?"),
+        Message(role="user", content="Comedy"),
+        Message(role="assistant", content="do you want a movie or series?"),
+        Message(role="user", content="Movie"),
+        Message(
+            role="user",
+            content="Generate a Google-like search query "
+            "text encompassing all previous chat questions and answers",
+        ),
+    ]
+    assert res == expected
+    qstr = search_query.build_string()
+    qstr_expected = textwrap.dedent(
+        """Human: You are a REST API that answers the question contained in <question> tags.
+Your response should be in the JSON which it's schema is specified in the <json> tags.
+
+<json>
+{
+    "google_like_search_term": {
+        "title": "Google Like Search Term",
+        "type": "string"
+    }
+}
+</json>
+<question>
+Human: Generate a Google-like search query text encompassing all previous chat questions and answers
+</question>
+
+Assistant:"""
+    )
+    assert qstr == qstr_expected
+    if run_bedrock:
         res = search_query(history="\n".join(history))
         assert isinstance(res, QueryGPTResponse)
