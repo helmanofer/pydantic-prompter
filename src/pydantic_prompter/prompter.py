cfb2631a626f5ee05461c51eac913c4af52dc4cd
Ofer Helman <ofer.helman@kaltura.com>
2023-09-02 23:52:48 +0300

    Support AWS Bedrock (#2)

--- src/pydantic_prompter/prompter.py
+++ src/pydantic_prompter/prompter.py
@@ -1,19 +1,24 @@
+import json
 import logging
 import os
 from typing import Dict, List, Any
 
 import yaml
 from jinja2 import Template
-from openai.error import AuthenticationError
 from pydantic import BaseModel, ValidationError
 from retry import retry
 
 from pydantic_prompter.exceptions import (
     OpenAiAuthenticationError,
     Retryable,
-    FailedToCastLLMResult, NonRetryable, )
+    FailedToCastLLMResult,
+    NonRetryable,
+    BedRockAuthenticationError,
+)
+from pydantic_prompter.settings import Settings
 
 logger = logging.getLogger()
+settings = Settings()
 
 
 class Message(BaseModel):
@@ -28,8 +33,19 @@ class LLM:
     def __init__(self, model_name):
         self.model_name = model_name
 
+    def debug_prompt(self, messages: List[Message], scheme: dict):
+        raise NotImplementedError
+
     def call(self, messages: List[Message], scheme: Dict) -> str:
         raise NotImplementedError
+
+    @classmethod
+    def get_llm(cls, llm: str, model_name: str):
+        if llm == "openai":
+            return OpenAI(model_name)
+        elif llm == "bedrock" and model_name.startswith("anthropic"):
+            return BedRockAnthropic(model_name)
+        raise Exception(f"Model not implemented {llm}, {model_name}")
 
 
 class OpenAI(LLM):
@@ -38,14 +54,18 @@ class OpenAI(LLM):
         openai_msgs = [item.dict() for item in msgs]
         return openai_msgs
 
+    def debug_prompt(self, messages: List[Message], scheme: dict) -> str:
+        return "\n".join(map(str, self.to_openai_format(messages)))
+
     def call(self, messages: List[Message], scheme: dict) -> str:
         import openai
+        from openai.error import AuthenticationError
 
         _function_call = {
             "name": scheme["name"],
         }
         messages = self.to_openai_format(messages)
-        openai.api_key = os.getenv("OPENAI_API_KEY")
+        openai.api_key = settings.openai_api_key
         try:
             chat_completion = openai.ChatCompletion.create(
                 model=self.model_name,
@@ -56,6 +76,60 @@ class OpenAI(LLM):
         except AuthenticationError as e:
             raise OpenAiAuthenticationError(e)
         return chat_completion.choices[0].message["function_call"]["arguments"]
+
+
+class BedRockAnthropic(LLM):
+    def debug_prompt(self, messages: List[Message], scheme: dict) -> str:
+        return self.build_prompt(messages, scheme)
+
+    @staticmethod
+    def to_anthropic_format(msgs: List[Message]):
+        role_converter = {"user": "Human", "system": "Human", "assistant": "Assistant"}
+        output = []
+        for msg in msgs:
+            output.append(f"{role_converter[msg.role]}: {msg.content}")
+        return "\n".join(output)
+
+    def build_prompt(self, messages: List[Message], scheme: dict):
+        ant_template = open(settings.template_paths.anthropic).read()
+        ant_scheme = json.dumps(scheme["parameters"]["properties"], indent=4)
+        ant_msgs = self.to_anthropic_format(messages)
+        template = Template(ant_template, keep_trailing_newline=True)
+        content = template.render(schema=ant_scheme, question=ant_msgs).strip()
+        return content
+
+    def call(self, messages: List[Message], scheme: dict) -> str:
+        content = self.build_prompt(messages, scheme)
+
+        body = json.dumps(
+            {
+                "max_tokens_to_sample": 200,
+                "prompt": content,
+                "stop_sequences": ["Human:"],
+                "temperature": 0.7,
+            }
+        )
+        logger.info(body)
+        try:
+            import boto3
+
+            session = boto3.Session(
+                profile_name=settings.aws_profile,
+                region_name=settings.aws_region,
+            )
+            client = session.client("bedrock")
+            response = client.invoke_model(
+                body=body,
+                modelId="anthropic.claude-instant-v1",
+                accept="application/json",
+                contentType="application/json",
+            )
+        except Exception as e:
+            raise BedRockAuthenticationError(e)
+
+        response_body = json.loads(response.get("body").read().decode())
+        logger.info(response_body)
+        return response_body.get("completion")
 
 
 class AnnotationParser:
@@ -122,7 +196,7 @@ class _Pr:
 
     def build_string(self, **inputs):
         msgs = self.build_prompt(**inputs)
-        return "\n".join(map(str, msgs))
+        return self.llm.debug_prompt(msgs, self.parser.llm_schema())
 
     def build_prompt(self, **inputs) -> List[Message]:
         if self.jinja:
@@ -132,7 +206,8 @@ class _Pr:
             content = self.function.__doc__.format(**inputs)
 
         import re
-        pattern = r'\n\s*- '
+
+        pattern = r"\n\s*- "
         parts = re.split(pattern, content)
 
         messages = []
@@ -155,8 +230,7 @@ class _Pr:
 class Prompter:
     def __init__(self, llm: str, model_name: str, jinja=False):
         self.jinja = jinja
-        if llm == "openai":
-            self.llm = OpenAI(model_name)
+        self.llm = LLM.get_llm(llm=llm, model_name=model_name)
 
     def __call__(self, function):
         return _Pr(function=function, jinja=self.jinja, llm=self.llm)
