0f1b0d63d39afa2401a2ef99f07f1624e86f3506
Ofer Helman <ofer.helman@IL-OFERHEL-RND.local>
2024-02-08 23:59:48 +0200

    cohere docs
    Signed-off-by: Ofer Helman <ofer.helman@IL-OFERHEL-RND.local>

--- docs/ai_services.md
+++ docs/ai_services.md
@@ -3,11 +3,25 @@
 If you want to use AWS Bedrock you should install bedrock-python-sdk
 
 ```bash
-pip install https://d2eo22ngex1n9g.cloudfront.net/Documentation/SDK/bedrock-python-sdk.zip
+pip install 'pydantic-prompter[bedrock]'
 ```
 
 setup your AWS [creds](https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-envvars.html) 
 
-```py hl_lines="16"
+```py hl_lines="17"
 --8<-- "examples/bedrock.py"
+```
+
+## Using Cohere
+
+If you want to use AWS Bedrock you should install bedrock-python-sdk
+
+```bash
+pip install 'pydantic-prompter[cohere]'
+```
+
+setup your AWS [creds](https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-envvars.html) 
+
+```py hl_lines="17"
+--8<-- "examples/cohere.py"
 ```
---
+++ examples/cohere.py
@@ -0,0 +1,26 @@
+from pydantic_prompter import Prompter
+from pydantic import BaseModel, Field
+from typing import List
+import os
+
+os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
+os.environ["AWS_ACCESS_KEY_ID"] = "..."
+os.environ["AWS_SECRET_ACCESS_KEY"] = "..."
+os.environ["AWS_SESSION_TOKEN"] = "..."
+
+
+class MyChildren(BaseModel):
+    num_of_children: int
+    children_names: List[str] = Field(description="The names of my children")
+
+
+@Prompter(llm="cohere", model_name="command")
+def me_and_mu_children(name) -> MyChildren:
+    """
+    - user: hi, my name is {name} and my children are called, aa, bb, cc, dd, ee
+    - user: |
+        how many children do I have and what's their names?
+    """
+
+
+print(me_and_mu_children(name="Zud"))
